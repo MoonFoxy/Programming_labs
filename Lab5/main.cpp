ecc7ac584ba7e5a5849fbf918b01825990262ebe
Ilya Balakirev <IlyaBalakirev2003@gmail.com>
2020-11-17 21:09:27 +0300

    Fix 5^2 = 24

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -33,7 +33,7 @@ int main()
     {
         for (int j = 0; j < 3; j++)
         {
-            int result = pow(array[i][j], power);
+            float result = pow(array[i][j], power);
             cout << result << " ";
         }
 
