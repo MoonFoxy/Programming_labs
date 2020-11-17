ac8bcc9ef0bb5d59c333020b1b0ca929932efaf2
MoonFoxy <IlyaBalakirev2003@gmail.com>
2020-11-17 19:14:36 +0300

    Mini fix

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -21,8 +21,8 @@ int main()
 
     for (int i = 3; i <= N; i += 2)
     {
-        bool prime = true;
-        for (int j = 3; j * j <= i; j += 2 )
+        bool prime = false;
+        for (int j = 3; j * j <= i; j += 2)
         {
             if ((i % j) == 0)
             {
