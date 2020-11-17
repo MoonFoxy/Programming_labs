ba3b4a60a150c66f9f8d368b75946ccdae9fdd75
MoonFoxy <IlyaBalakirev2003@gmail.com>
2020-11-17 19:14:50 +0300

    New 5 lab

---
+++ Lab5/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+  Ввод:
+  С клавиатуры вводится двумерный массив целых чисел размерностью 3х3. Затем вводится натуральная n.
+  Вывод:
+  Необходимо возвести матрицу в степень n
+*/
+
+int main()
+{
+    int array[3][3];
+
+    cout << "Enter numbers: ";
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> array[i][j];
+        }
+    }
+
+    int power;
+
+    cout << "Enter power: ";
+    cin >> power;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            int result = pow(array[i][j], power);
+            cout << result << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
