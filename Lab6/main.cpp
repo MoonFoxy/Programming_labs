ace8640157f03d908153c14c148399474d521324
MoonFoxy <IlyaBalakirev2003@gmail.com>
2020-11-26 22:40:53 +0300

    Done Lab6

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-using namespace std;
+#include <cstring>
 
 /*
   С клавиатуры вводится произвольная строка длиной не более 80 символов. Затем вводится ключ - подстрока, длиной не более 10 символов.
@@ -14,5 +13,71 @@ using namespace std;
 
 int main()
 {
+    const int textSize = 80;
+    const int keySize = 10;
 
+    char text[textSize] = { "" };
+    char key[keySize] = { "" };
+
+    std::cout << "Enter text: ";
+    std::cin.getline(text, textSize);
+
+    std::cout << "Enter key: ";
+    std::cin.getline(key, keySize);
+
+    size_t textLen = strlen(text);
+    size_t keyLen = strlen(key);
+
+    int matchCount = 0;
+    std::string keyReplaced;
+    for (int i = 0; text[i];)
+    {
+        if (text[i] == key[0])
+        {
+            if (std::string(text).substr(i, keyLen) == key)
+            {
+                keyReplaced += "\"";
+                keyReplaced += key;
+                keyReplaced += "\"";
+                matchCount++;
+                i += keyLen;
+                continue;
+            }
+        }
+        keyReplaced += text[i];
+        i++;
+    }
+
+    std::cout << "(1): " << key << " - " << matchCount << "\n";
+    std::cout << "(2): " << keyReplaced << "\n";
+
+    int maxCount = 0;
+    char maxChar;
+    for (int i = 0; i < textLen; i++)
+    {
+        int count = 0;
+        for (int j = i; j < textLen; j++)
+        {
+            if (text[i] == text[j]) count++;
+        }
+        if (count > maxCount)
+        {
+            maxCount = count;
+            maxChar = text[i];
+        }
+    }
+
+    std::cout << "(3): " << maxChar << " - " << maxCount << "\n";
+    std::cout << "(4):";
+
+    char charts[9] = " ,.-!?:;";
+    char* delim = strtok(text, charts);
+    while (delim)
+    {
+        std::cout << "\t" << delim << ",\n";
+        delim = strtok(NULL, charts);
+    }
+
+    system("pause");
+    return 0;
 }
