75bb18deac731fff31d4d5fb3bec69a7265578cb
MoonFoxy <IlyaBalakirev2003@gmail.com>
2021-02-04 19:51:33 +0300

    i dont understand

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -31,21 +31,27 @@ int main()
 
     unsigned long long aullResult[MATRIX_SIZE][MATRIX_SIZE] = { 0 };
 
-    for (int iP = 1; iP < iPower; iP++)
+    for (; iPower > 1; --iPower)
+    {
         for (int iI = 0; iI < MATRIX_SIZE; iI++)
             for (int iJ = 0; iJ < MATRIX_SIZE; iJ++)
                 for (int iK = 0; iK < MATRIX_SIZE; iK++)
                     aullResult[iI][iJ] += aullArray[iK][iJ] * aullTemp[iI][iK];
 
+        for (int iI = 0; iI < MATRIX_SIZE; iI++)
+            for (int iJ = 0; iJ < MATRIX_SIZE; iJ++)
+                aullTemp[iI][iJ] = aullResult[iI][iJ];
+    }
+
     for (int iI = 0; iI < MATRIX_SIZE; iI++)
     {
         for (int iJ = 0; iJ < MATRIX_SIZE; iJ++)
-        {
             std::cout << aullResult[iI][iJ] << " ";
-        }
 
         std::cout << "\n";
     }
+
+    system("pause");
 
     return 0;
 }
