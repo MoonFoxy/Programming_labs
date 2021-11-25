6c926c882c6075ff4df808aff04a4749d499924e
MoonFoxy <IlyaBalakirev2003@gmail.com>
2021-11-25 14:19:18 +0300

    Change comments

--- 2. Computer organization Course 2/Semester 3/Lab 5/main.cpp
+++ 2. Computer organization Course 2/Semester 3/Lab 5/main.cpp
@@ -4,12 +4,12 @@
 #include <conio.h>
 // #include <bios.h>
 
-unsigned long far *count =  (unsigned long far*) MK_FP(0x0040, 0x006c); // Тут храниться время (Кол-во выполненных прерываний)
+unsigned long far *count =  (unsigned long far*) MK_FP(0x0040, 0x006c); // Тут храниться время (Кол-во выполненных прерываний) Дальний указатель
 
-void interrupt mybeep(...) // Прерывание
+void interrupt mybeep(...)	// Прерывание
 {
 	static char a = 0;
-	if (*count % 18L == 0)
+	if (*count % 18L == 0)	// [ 18L ] Тип данных: double; Значение: long double;
 	{
 		switch(a)
 		{
@@ -48,14 +48,14 @@ void interrupt mybeep(...) // Прерывание
 }
 
 
-int main(void)
+int main()
 {
     clrscr();
-    void interrupt (*oldvect)(...);		// Переменная
-    oldvect = getvect(0x1c);	// Получаем значение вектора прерываний
-    setvect(0x1c, mybeep);		// Создаем свое прерывание
+    void interrupt (*oldvect)(...);		// Указатель на прерывание
+    oldvect = getvect(0x1c);			// Получаем значение старого вектора прерываний
+    setvect(0x1c, mybeep);				// Создаем свое прерывание
 	printf("To exit, press any key...\n");
 	getch();
-    setvect(0x1c, oldvect);		// Восстанавливаем значения
+    setvect(0x1c, oldvect);				// Восстанавливаем значение старого вектора прерываний
     return 0;
 }
